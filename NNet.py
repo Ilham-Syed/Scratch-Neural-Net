4ecbcfeb23d24b650c06464abe060f98a4d3af95
Ilham <ilhamsyed1310@gmail.com>
2025-12-08 15:19:25 +0530

    Added softmax activation function

--- NNet.py
+++ NNet.py
@@ -10,8 +10,6 @@ X=[[1,2,3,2.5],
    [ -1.5,2.7,3.3,-0.8]]
 '''
 
-X,y=spiral_data(100,3)
-
 class Layer_Dense:
     def __init__(self,n_inputs,n_neurons):
         self.weights= 0.10* np.random.randn(n_inputs,n_neurons) #No need to transponse 
@@ -23,13 +21,29 @@ class Activation_ReLU:
     def forward(self,inputs):
         self.output=np.maximum(0,inputs)
 
-#Creating the neural network layers
+class Activation_Softwax:
+    def forward(self,inputs):
+        exp_values=np.exp(inputs - np.max(inputs,axis=1,keepdims=True))
+        probabilities=exp_values / np.sum(exp_values,axis=1,keepdims=True)
+        self.output=probabilities
 
-layer1=Layer_Dense(2,5)
-layer1.forward(X)
 
+X,y=spiral_data(samples=100,classes=3)
+
+dense1=Layer_Dense(2,3)
 activation1=Activation_ReLU()
-activation1.forward(layer1.output)
 
-print(activation1.output)
+dense2=Layer_Dense(3,3)
+activation2=Activation_Softwax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+
+
 
